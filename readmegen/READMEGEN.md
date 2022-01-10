728a9b6cf1bd4134bd1dc63bb212f74e2c712205
Lo Selby <laurenxselby@gmail.com>
2022-01-09 22:41:13 -0800

    took out input question for bug contact - updated readme content with less fields and fixed bugs related to broken links and unecessary questions

---
+++ readmegen/READMEGEN.md
@@ -0,0 +1,58 @@
+
+# Title 
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-lightblue.svg)](https://opensource.org/licenses)
+
+[GitHub Repo](https://github.com/lonewolfco) | [GitHub Page](https://github.com/lonewolfco)
+
+## Table of Contents
+- [Description](#description)
+- [Usage](#usage)
+- [Installation Instructions](#install)
+- [Languages Used](#languages)
+- [Contributors](#contributors)
+- [Report a Bug / Questions?](#bug)
+- [Questions?](#questions)
+- [License](#license)
+
+---
+
+## [Description](#description)
+<a name="description"></a>
+description.
+
+
+## [Usage](#usage)
+<a name="usage"></a>
+The user can use this application by usage.
+
+## [Installation Instructions](#install)
+<a name="install"></a>
+Title  can be accessed by: 
+install
+
+## [Languages Used](#languages)
+<a name="languages"></a>
+js node inquirer
+
+## [Contributors](#contributors)
+<a name="contributors"></a>
+Lo
+
+Want to be apart of this project?
+- Contact: Lo
+
+
+## [Report a Bug / Questions?](#bug)
+<a name="bug"></a>
+Report a bug with the app by contacting the contributors on the repo:
+- [GitHub Repo](https://github.com/lonewolfco) 
+- [GitHub User: lonewolfco](https://github.com/lonewolfco)
+- loselby@icloud.com
+
+
+## [License](#license)
+<a name="license"></a>
+fjdksa;
+    
+    
---
+++ readmegen/readmeContent.js
@@ -0,0 +1,64 @@
+// filler content for ReadMe
+
+
+const generateReadMe = (answers) => {
+    return `
+# ${answers.projectTitle}\n
+[![License: ${answers.licenseBadge}](https://img.shields.io/badge/License-${answers.licenseBadge}-${answers.badgeColor}.svg)](https://opensource.org/licenses)
+
+[GitHub Repo](${answers.repo}) | [GitHub Page](${answers.page})
+
+## Table of Contents
+- [Description](#description)
+- [Usage](#usage)
+- [Installation Instructions](#install)
+- [Languages Used](#languages)
+- [Contributors](#contributors)
+- [Report a Bug / Questions?](#bug)
+- [Questions?](#questions)
+- [License](#license)
+
+---
+
+## [Description](#description)
+<a name="description"></a>
+${answers.description}.
+
+
+## [Usage](#usage)
+<a name="usage"></a>
+The user can use this application by ${answers.usage}.
+
+## [Installation Instructions](#install)
+<a name="install"></a>
+${answers.projectTitle} can be accessed by: 
+${answers.install}
+
+## [Languages Used](#languages)
+<a name="languages"></a>
+${answers.languages}
+
+## [Contributors](#contributors)
+<a name="contributors"></a>
+${answers.contributors}
+
+Want to be apart of this project?
+- Contact: ${answers.contributorContact}
+
+
+## [Report a Bug / Questions?](#bug)
+<a name="bug"></a>
+Report a bug with the app by contacting the contributors on the repo:
+- [GitHub Repo](${answers.repo}) 
+- [GitHub User: ${answers.githubUsername}](https://github.com/${answers.githubUsername})
+- ${answers.email}
+
+
+## [License](#license)
+<a name="license"></a>
+${answers.licenseContent}
+    
+    `
+  }
+
+    module.exports = generateReadMe;
